d635b534a50f644619ff7cb81adb0119429165e2
Jessica Severino <jessicaa.severinoo@gmail.com>
2018-11-24 16:51:50 -0200

    exclusoes de components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,24 +2,23 @@ import { PostsComponent } from './posts/posts.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PerfilComponent } from './perfil/perfil.component';
-import { CadastroQuestaoComponent } from './cadastro-tarefa/cadastro-questao/cadastro-questao.component';
-import { CadastroTarefaComponent } from './cadastro-tarefa/cadastro-tarefa.component';
 import { ResolverQuestoesComponent } from './resolver-questoes/resolver-questoes.component';
 import { NiveisQuestoesComponent } from './niveis-questoes/niveis-questoes.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { ProgressoTurmaComponent } from './progresso-turma/progresso-turma.component';
 import { LoginComponent } from './login/login.component';
+import { CadastroTurmasComponent } from './cadastro-turmas/cadastro-turmas.component';
 
 export const routes: Routes = [
   { path: 'perfil', component: PerfilComponent },
-  { path: 'tarefas', component: CadastroTarefaComponent },
   { path: 'cadastro', component: CadastroComponent },
   { path: 'alunos', component: CadastroComponent },
   { path: 'progresso', component: ProgressoTurmaComponent },
   { path: 'resolver-questoes', component: ResolverQuestoesComponent },
   { path: 'niveis', component: NiveisQuestoesComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'posts', component: PostsComponent }
+  { path: 'posts', component: PostsComponent },
+  { path: 'cadastro-turmas', component: CadastroTurmasComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,12 @@ import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { AuthService } from 'src/auth.service';
+
 import { SairComponent } from './sair/sair.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { MenuComponent } from './menu/menu.component';
 import { ProgressoTurmaComponent } from './progresso-turma/progresso-turma.component';
-import { CadastroTarefaComponent } from './cadastro-tarefa/cadastro-tarefa.component';
-import { CadastroQuestaoComponent } from './cadastro-questao/cadastro-questao.component';
 import { NiveisQuestoesComponent } from './niveis-questoes/niveis-questoes.component';
-import { TarefasComponent } from './tarefas/tarefas.component';
-import { CadastroQuestoesComponent } from './tarefas/cadastro-questoes/cadastro-questoes.component';
-import { VerQuestoesComponent } from './tarefas/ver-questoes/ver-questoes.component';
-import { NiveisComponent } from './tarefas/niveis/niveis.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { ResolverQuestoesComponent } from './resolver-questoes/resolver-questoes.component';
 import { LoginComponent } from './login/login.component';
@@ -35,18 +30,12 @@ import { CadastroTurmasComponent } from './cadastro-turmas/cadastro-turmas.compo
     CadastroComponent,
     MenuComponent,
     ProgressoTurmaComponent,
-    CadastroTarefaComponent,
-    CadastroQuestaoComponent,
-    NiveisQuestoesComponent,
-    TarefasComponent,
-    CadastroQuestoesComponent,
-    VerQuestoesComponent,
-    NiveisComponent,
     PerfilComponent,
     ResolverQuestoesComponent,
     LoginComponent,
     PostsComponent,
-    CadastroTurmasComponent
+    CadastroTurmasComponent,
+    NiveisQuestoesComponent
   ],
   imports: [
     BrowserModule,
